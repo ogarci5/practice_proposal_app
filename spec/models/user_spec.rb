60a1992f18c851ba5033ff4b2d4fb4813edde841
Oliver Garcia <ogarci5@gmail.com>
2013-06-10 17:40:05 -0500

    updated tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 describe User do
 
-  let(:user) { FactoryGirl.create(:user) }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
   
-  subject { user }
+  
+  subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
