5c7eaaa20342828c27f70f15eb8b83e54277faf8
Oliver Garcia <ogarci5@gmail.com>
2013-06-26 13:45:38 -0500

    Fixe name bug

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,4 @@
-class ApiController < ApplicationController
+class APIController < ApplicationController
   skip_before_filter  :verify_authenticity_token
   respond_to :html, :json
   include ProposalsHelper
